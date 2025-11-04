b0fd010fb3b8eb3800196d5fa8fd56e5273cddb1
Sam Aaron <samaaron@gmail.com>
2025-11-04 14:10:45 +0100

    Version - 0.1.4

--- packages/supersonic-scsynth-synthdefs/index.js
+++ packages/supersonic-scsynth-synthdefs/index.js
@@ -15,7 +15,7 @@ export function getSynthDefPath(name) {
 }
 
 // CDN URL for browser usage (unpkg)
-export const CDN_BASE = 'https://unpkg.com/supersonic-scsynth-synthdefs@0.1.3';
+export const CDN_BASE = 'https://unpkg.com/supersonic-scsynth-synthdefs@0.1.4';
 export const SYNTHDEFS_CDN = `${CDN_BASE}/synthdefs`;
 
 export default {
