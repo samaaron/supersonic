42a2eaa65b42f178a4418dcdf84cd818a1f55a0e
Sam Aaron <samaaron@gmail.com>
2025-12-12 14:50:23 +1100

    Version - 0.15.4

--- src/audio_processor.cpp
+++ src/audio_processor.cpp
@@ -84,7 +84,7 @@ extern "C" {
 // Supersonic version - appended to SC version
 static const int SUPERSONIC_VERSION_MAJOR = 0;
 static const int SUPERSONIC_VERSION_MINOR = 15;
-static const int SUPERSONIC_VERSION_PATCH = 3;
+static const int SUPERSONIC_VERSION_PATCH = 4;
 
 // Global pointers
 extern "C" {
