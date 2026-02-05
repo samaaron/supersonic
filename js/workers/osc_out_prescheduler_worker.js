3f306126c956d0fa10647e79878dc08f7895f192
Sam Aaron <samaaron@gmail.com>
2026-02-05 12:43:16 +0000

    License - make workers MIT/GPL and describe architectural intent wrt licensing

--- js/workers/osc_out_prescheduler_worker.js
+++ js/workers/osc_out_prescheduler_worker.js
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: MIT OR GPL-3.0-or-later
 // Copyright (c) 2025 Sam Aaron
 
 import * as MetricsOffsets from '../lib/metrics_offsets.js';
